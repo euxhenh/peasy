b778c9f299505f70621e627ebbc980bd306843b3
euxhenh <ehasanaj@cs.cmu.edu>
2023-03-27 16:31:56 -0400

    Adding notebook

--- src/peasy/_colony.py
+++ src/peasy/_colony.py
@@ -2,7 +2,6 @@ import logging
 from typing import List, Tuple
 
 import matplotlib.pyplot as plt
-import matplotlib.transforms as mtransforms
 import seaborn as sns
 
 from ._artist import Artist, MultiArtist
